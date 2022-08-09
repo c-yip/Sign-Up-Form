9b799049a61d9c7f854f01c84689c57c21feebae
Christian <christianyip92@gmail.com>
2022-08-09 14:29:09 -0700

    Add error message for passwords not matching.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", () => {
-    const password = document.querySelector("#password").value;
-    const password_confirm = document.querySelector("#password_confirm").value;
+form.addEventListener("submit", e => {
+    const password_value = document.querySelector("#password").value;
+    const password_confirm_value = document.querySelector("#password_confirm").value;
 
-    console.log(password);
-    console.log(password_confirm);
+    const password = document.querySelector("#password");
+    const password_confirm = document.querySelector("#password_confirm");
 
-    if (password !== password_confirm) {
-        alert("No bueno")
+    if (password_value !== password_confirm_value) {
+        e.preventDefault();
+        password.classList.add("error");
+        password_confirm.classList.add("error");
+        password.setCustomValidity("Passwords must match.");
+        password.reportValidity();
     }
 });
