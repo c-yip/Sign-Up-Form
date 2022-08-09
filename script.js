ffe8ba65165a477d6d523c16f5b6c8b477251614
Christian <christianyip92@gmail.com>
2022-08-09 14:10:32 -0700

    Fix bug re value of input not being fetched.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const form = document.querySelector("form");
 
 form.addEventListener("submit", () => {
-    const password = document.querySelector("#password");
-    const password_confirm = document.querySelector("#password_confirm");
+    const password = document.querySelector("#password").value;
+    const password_confirm = document.querySelector("#password_confirm").value;
 
     console.log(password);
     console.log(password_confirm);
